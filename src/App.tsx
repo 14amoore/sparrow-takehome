b1ac3942aa81ae6949153c770abb252b4a964f27
Adam Moore <adamoore@gmail.com>
2024-11-14 15:55:34 -0500

    routes working

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,17 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import EmployeeView from './views/EmployeeView';
+import CustomerView from './views/CustomerView';
+
 function App() {
-  return <div>hi</div>;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/employee" element={<EmployeeView />} />
+        <Route path="/customer" element={<CustomerView />} />
+        {/* Define other routes here */}
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
