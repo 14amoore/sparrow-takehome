9d6ee22789de98f4a6ec59d55f670d524fd6ad30
Adam Moore <adamoore@gmail.com>
2024-11-14 15:20:04 -0500

    ready for merge

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,5 @@
-import { HashRouter as Router, Route, Routes } from 'react-router-dom';
-import EmployeeView from './views/EmployeeView';
-import CustomerView from './views/CustomerView';
-
 function App() {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/employee" Component={EmployeeView} />
-        <Route path="/customer" Component={CustomerView} />
-        <Route />
-      </Routes>
-    </Router>
-  );
+  return <div>hi</div>;
 }
 
 export default App;
