49b095272cd4ad32b38f5e612f5f6940b62ad2c5
Adam Moore <adamoore@gmail.com>
2024-11-14 15:11:44 -0500

    switching to hashrouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import EmployeeView from './views/EmployeeView';
 import CustomerView from './views/CustomerView';
 
